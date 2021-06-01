f984b3ed07954369f2044d713bdc3bc0ac234856
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-01 13:04:29 +0530

    Checking if the both lines are equal in distance or not.

--- LineComparison.java
+++ LineComparison.java
@@ -1,11 +1,12 @@
 package linecomparison;
 import java.util.*;
+import java.lang.*;
 
 public class LineComparison 
 {
 	public static void main(String[] args) 
 	{
-		float x1=0, x2=0, y1=0, y2=0;
+		float x1=0, x2=0, y1=0, y2=0, a1=0, a2=0, b1=0, b2=0;
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the value of a point x1:"); 
 		x1 = scanner.nextFloat();
@@ -15,9 +16,27 @@ public class LineComparison
 		x2 = scanner.nextFloat();
 		System.out.println("Enter the value of a point y2:"); 
 		y2 = scanner.nextFloat();
-		//System.out.println(x1 + " " + x2 + " " + y1 + " " + y2);
-		float length =(float) Math.sqrt( ( ( x2 - x1 ) * ( x2 - x1 ) ) + ( ( y2 - y1 ) * ( y2 - y1 ) ) );
-		System.out.println("The length of a line is:" + length);
+		System.out.println("Enter the value of a point a1:"); 
+		a1 = scanner.nextFloat();
+		System.out.println("Enter the value of a point b1:"); 
+		b1 = scanner.nextFloat();
+		System.out.println("Enter the value of a point a2:"); 
+		a2 = scanner.nextFloat();
+		System.out.println("Enter the value of a point b2:"); 
+		b2 = scanner.nextFloat();
+		float firstLineLength =(float) Math.sqrt( ( ( x2 - x1 ) * ( x2 - x1 ) ) + ( ( y2 - y1 ) * ( y2 - y1 ) ) );
+		float secondLineLength =(float) Math.sqrt( ( ( a2 - a1 ) * ( a2 - a1 ) ) + ( ( b2 - b1 ) * ( b2 - b1 ) ) );
+		System.out.println("The length of a line is:" + firstLineLength);
+		System.out.println("The length of a line is:" + secondLineLength);
+		Float obj1 = firstLineLength;
+		Float obj2 = secondLineLength;
+		if (obj1.equals(obj2)) {
+			System.out.println("Both the line are equal");
+		}
+		else
+		{
+			System.out.println("they are not equal");
+		}
 		
 	}
 }
